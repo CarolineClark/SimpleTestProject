1025ef8317f9fbaea6aeb5bd032e92c603e9bf71
Caroline Clark <carolinejaneclark@gmail.com>
2018-02-24 23:44:57 +0000

    Added test project with example asset

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+	public Vector3 centre;
+	public float speed;
+	private Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		light.transform.RotateAround(centre, Vector3.forward, speed * Time.deltaTime);
+	}
+}
